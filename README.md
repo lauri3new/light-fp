00bbb800e3cbe9f04dd64c537a6639c439dbdee2
Laurence Newman <laurence@wearepercent.com>
2020-02-22 11:03:11 +0000

    some todos

--- README.md
+++ README.md
@@ -3,8 +3,9 @@
 # light-fp
 A basic Typescript functional programming (fp) library.
 
-This library provides typed general helper functions for Arrays, Objects, control flow, function composition, Option and Either data types amongst other things.
+// TODO: mention server, split modules up
+This library provides data types typed general as well as a few helper functions for Arrays, Objects, control flow, function composition.
 
 I created this library for myself as a learning exercise and also as a more basic, and hopefully more accessible alternative to some other typescript fp libraries.
 
-Inspiration from the excellent book *functional programming in scala* Manning, and the javascript fp library Ramda.
+Inspiration from the excellent book *functional programming in scala* Manning, Cats scala library and the javascript library Ramda.
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 
-export const curryTwo = <A, B, C>(f: (_:A, __:B) => C): (_:A) => (_:B) => C => (a: A) => (b: B) => f(a, b)
-export const uncurryTwo = <A, B, C>(f:(_:A) => (_:B) => C) => (a: A, b: B) => f(a)(b)
-export const composeTwo = <A, B, C>(f: (_:A) => B, g: (_:B) => C) => (a: A) => g(f(a))
+// export const curryTwo = <A, B, C>(f: (_:A, __:B) => C): (_:A) => (_:B) => C => (a: A) => (b: B) => f(a, b)
+// export const uncurryTwo = <A, B, C>(f:(_:A) => (_:B) => C) => (a: A, b: B) => f(a)(b)
+// export const composeTwo = <A, B, C>(f: (_:A) => B, g: (_:B) => C) => (a: A) => g(f(a))
 
-// control
-type Predicate<A> = (_:A) => boolean
-export const when = <A, B>(a:Predicate<A>) => (b: (_:A) => B) => (c: A) => {
-  if (a(c)) {
-    return b(c)
-  }
-  return c
-}
+// // control
+// type Predicate<A> = (_:A) => boolean
+// export const when = <A, B>(a:Predicate<A>) => (b: (_:A) => B) => (c: A) => {
+//   if (a(c)) {
+//     return b(c)
+//   }
+//   return c
+// }
+
+// TODO: change order of match, run, onComplete args.. put some handler first, none after
+// TODO: explore transformers e.g. optionT
+// TODO: module structure npm
