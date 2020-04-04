852ed29390b8b0f764e86ecec11ca3a86d622500
lauri3new <laurence@wearepercent.com>
2020-04-05 00:39:44 +0100

    fix validation middlewares

--- src/Server/validations.ts
+++ src/Server/validations.ts
@@ -1,26 +1,26 @@
-import yup from 'yup'
+import { ObjectSchema, ValidationError } from 'yup'
 import { Context } from './handler'
-import { Right, Left, Either } from '../Either'
+import { Right, Left } from '../Either'
 import { BadRequest, Result } from './result'
 import { PromiseEither } from '../PromiseEither'
 
 // TODO: tidy and find what yup actually returns on error for example
 
-const PEfromValidation = <A extends object>(a: yup.ObjectSchema<A>, input: any): PromiseEither<Result, A> => PromiseEither(a.validate(input)
-  .then(abd => Right(abd))
-  .catch(() => Left(BadRequest('asef'))))
+const PEfromValidation = <A extends object>(a: ObjectSchema<A>, input: any): PromiseEither<Result, A> => PromiseEither(a.validate(input)
+  .then(b => Right(b))
+  .catch((e) => Left(BadRequest(e))))
 
-export const query = <B extends object>(a: yup.ObjectSchema<B>) => <A extends Context>(ctx: A) => {
+export const query = (a: (_: ValidationError) => Result) => <B extends object>(b: ObjectSchema<B>) => <A extends Context>(ctx: A) => {
   const { query: _query } = ctx.req
-  return PEfromValidation(a, _query).map(q => ({ ...ctx, query: q }))
+  return PEfromValidation(b, _query).map(q => ({ ...ctx, query: q }))
 }
 
-export const param = <B extends object>(a: yup.ObjectSchema<B>) => <A extends Context>(ctx: A) => {
+export const param = (a: (_: ValidationError) => Result) => <B extends object>(b: ObjectSchema<B>) => <A extends Context>(ctx: A) => {
   const { param: _param } = ctx.req
-  return PEfromValidation(a, _param).map(p => ({ ...ctx, param: p }))
+  return PEfromValidation(b, _param).map(p => ({ ...ctx, param: p }))
 }
 
-export const body = <B extends object>(a: yup.ObjectSchema<B>) => <A extends Context>(ctx: A) => {
+export const body = (a: (_: ValidationError) => Result) => <B extends object>(b: ObjectSchema<B>) => <A extends Context>(ctx: A) => {
   const { body: _body } = ctx.req
-  return PEfromValidation(a, _body).map(b => ({ ...ctx, body: b }))
+  return PEfromValidation(b, _body).map(bd => ({ ...ctx, body: bd }))
 }
