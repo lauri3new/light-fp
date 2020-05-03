1c483663fd5437813604ddb8af15d81cfe72d174
lauri3new <laurence@wearepercent.com>
2020-05-03 09:19:59 +0100

    special k data type coming along well

--- src/SpecialKPE/index.ts
+++ src/SpecialKPE/index.ts
@@ -10,12 +10,12 @@ export interface SpecialKPE<C, E, A, I> {
   // leftMap: <EE>(f:(_:E) => EE) => SpecialKPE<C, EE, A>
   // flatMap: <G, EE, H>(f: SpecialKPE<G, EE, H>) => SpecialKPE<G, EE | E, H>
   // flatMapF: <EE, H extends object>(f:(_: A) => Promise<Either<EE, H>>) => SpecialKPE<C, EE | E, H>
-  // runWith: <I, J, K>(
-  //   context: C,
-  //   onSuccess: (_:A) => I,
-  //   onFailure: (_:E) => J,
-  //   onError: (_?: Error) => K
-  // ) => Promise<I | J | K>
+  runWith: <L, M, N>(
+    context: I,
+    onError: (_?: Error) => N,
+    onSuccess: (_:A) => L,
+    onFailure: (_:E) => M
+  ) => Promise<L | M | N>
 }
 
 export const SpecialKPE = <C, E, A, I>(val:(_: I) => Promise<[Either<E, A>, C]>): SpecialKPE<C, E, A, I> => ({
@@ -40,7 +40,7 @@ export const SpecialKPE = <C, E, A, I>(val:(_: I) => Promise<[Either<E, A>, C]>)
           }
         )
       )
-  )
+  ),
   // write: <D>(f:(_:C) => D) => SpecialKPE<C, G, E, A>((c: C) => val(c).then(([eitherA]) => [eitherA, f(c)]))
   // mapWithCtx: <B extends object>(f:(_:A) => B) => SpecialKPE<C, E, C & B>((c: C) => val(c).then((eitherA) => eitherA.map(f).map(a => ({ ...a, ...c })))),
   // leftMap: <EE>(f: (_:E) => EE) => SpecialKPE<C, EE, A>((c: C) => val(c).then(eitherA => eitherA.leftMap(f))),
@@ -51,27 +51,60 @@ export const SpecialKPE = <C, E, A, I>(val:(_: I) => Promise<[Either<E, A>, C]>)
   //       right => f.__val(right)
   //     ))
   // ),
-  // runWith: <I, J, K>(
-  //   c: C,
-  //   f: (_:A) => I,
-  //   g: (_:E) => J,
-  //   j: (_?: Error) => K
-  // ) => val(c).then(
-  //   (a) => a.match(
-  //     none => g(none),
-  //     some => f(some)
-  //   )
-  // ).catch(
-  //   j
-  // )
+  runWith: <L, M, N>(
+    c: I,
+    j: (_?: Error) => N,
+    f: (_:A) => L,
+    g: (_:E) => M
+  ) => val(c).then(
+    ([a, _c]) => a.match(
+      none => g(none),
+      some => f(some)
+    )
+  ).catch(
+    j
+  )
 })
+
+type Service = { userService: { get: () => 2 } }
+
+const SpecialK = SpecialKPE<Service, never, number, Service>(async (c) => [Right(1), c])
+const specialK = <C>() => SpecialKPE<C, never, undefined, C>(async (cc) => [Right(undefined), cc])
+
+// specialK<Service>()
+//   .flatMapF(async (a, g) => Right(123))
 
 type User = {
   name: string
   id: number
 }
 
-const b = SpecialKPE(async () => [Right(1), 'hello'])
-  .map(a => 2)
-  .write(async g => 123)
-  .flatMapF((f, g) => )
+SpecialK
+  .map((a) => {
+    console.log('hiya')
+    return a
+  })
+  .runWith(
+    { userService: { get: () => 2 } },
+    a => console.log('error', a),
+    a => console.log('success', a),
+    a => console.log('failure', a)
+  )
+
+const yela = specialK<Service>()
+  .flatMapF(async (_, ctx) => Right(ctx.userService.get()))
+
+setTimeout(() => {
+  yela.runWith(
+    { userService: { get: () => 2 } },
+    a => console.log('error', a),
+    a => console.log('success', a),
+    a => console.log('failure', a)
+  )
+}, 5000)
+
+// const b = SpecialKPE(async (c: { userService: { get: () => 2 } }) => [Right(1), c])
+//   .map(a => 2)
+//   .flatMapF(async (f, g) => Right('wasup'))
+//   .flatMapF(async (f, g) => Right(g.userService.get()))
+//   .write(async (g) => ({ ...g, mySecondService: { get: () => 214 } }))
